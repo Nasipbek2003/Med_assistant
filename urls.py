61b2ae227a1a2582ae26a85a216be08ceb3392c5
Nasipbek2003 <abdrahmanovnasi@gmail.com>
2025-06-22 22:11:40 +0600

    Очистко от ненужних файлов

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
- 
-urlpatterns = [
-    # ... существующие URL ...
-    path('chat/doctor/send/', views.doctor_send_message, name='doctor_send_message'),
-] 
